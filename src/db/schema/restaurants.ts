efbc2874cfc7cb1f03ca6bdd64163491146c4892
Tiago Behenck dos Santos <tiago.behenck.santos@gmail.com>
2024-02-27 15:07:04 -0300

    create the relations between tables  restaurants and user (restaurant_manager)

--- src/db/schema/restaurants.ts
+++ src/db/schema/restaurants.ts
@@ -1,10 +1,26 @@
-import { text, timestamp,pgTable, pgEnum } from 'drizzle-orm/pg-core';
+import { text, timestamp,pgTable } from 'drizzle-orm/pg-core';
 import { createId } from '@paralleldrive/cuid2'
 
-export const restaurant = pgTable('restaurants', {
+import { users } from './users'
+import { relations } from 'drizzle-orm';
+
+export const restaurants = pgTable('restaurants', {
     id: text('id').$defaultFn(() => createId()).primaryKey(),
     name: text('name').notNull(),
     description: text('description'),
+    managerId: text('manager_id').references(() => users.id, { 
+        onDelete: 'set null'
+    }),
     createdAt: timestamp('created_at').notNull().defaultNow(),
     updatedAt: timestamp('updated_at').notNull().defaultNow(),
 });
+
+export const restaurantsRelations = relations(restaurants, ({ one }) => {
+    return {
+      manager: one(users, {
+        fields: [restaurants.managerId],
+        references: [users.id],
+        relationName: 'restaurant_manager',
+      })
+    }
+})
