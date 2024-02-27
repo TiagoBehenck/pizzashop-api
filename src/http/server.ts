96f0d17510023bfb1fb3c0e0b9f0dbe682098813
Tiago Behenck dos Santos <tiago.behenck.santos@gmail.com>
2024-02-27 14:11:18 -0300

    initial commit

---
+++ src/http/server.ts
@@ -0,0 +1,10 @@
+import { Elysia } from 'elysia'
+
+const app = new Elysia()
+    .get('/', () => { 
+        return 'Teste'
+    })
+
+app.listen(3333, () => { 
+    console.log('HTTP server running')
+})
