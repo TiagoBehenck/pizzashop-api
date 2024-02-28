ce48faa83045ff7c8549ad88f203d46645970974
Tiago Behenck dos Santos <tiago.behenck.santos@gmail.com>
2024-02-28 15:33:26 -0300

    create file to connection with database

---
+++ src/db/connection.ts
@@ -0,0 +1,10 @@
+import postgres from 'postgres'
+import { drizzle } from 'drizzle-orm/postgres-js'
+
+import * as schema from './schema'
+import { env } from '../env'
+
+const connectionString = env.DATABASE_URL
+
+const connection = postgres(connectionString)
+export const db = drizzle(connection, { schema });
