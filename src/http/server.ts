85c78a29468ab5ae8494701b13118b0af1c6c315
Tiago Behenck dos Santos <tiago.behenck.santos@gmail.com>
2024-02-29 14:38:21 -0300

    create the route to add new restaurant

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,7 +1,28 @@
 import { Elysia } from 'elysia'
+import { db } from '../db/connection'
+import { restaurants, users } from '../db/schema'
 
-const app = new Elysia().get('/', () => {
-  return 'Teste'
+const app = new Elysia().post('/restaurants', async ({ body, set }) => {
+  const { restaurantName, name, email, phone } = body as any
+
+  const [manager] = await db
+    .insert(users)
+    .values({
+      name,
+      email,
+      phone,
+      role: 'manager',
+    })
+    .returning({
+      id: users.id,
+    })
+
+  await db.insert(restaurants).values({
+    name: restaurantName,
+    managerId: manager.id,
+  })
+
+  set.status = 204
 })
 
 app.listen(3333, () => {
