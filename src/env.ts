c22d8eb11fb72526e6c0e0069a6ec21347fdeb60
Tiago Behenck dos Santos <tiago.behenck.santos@gmail.com>
2024-03-01 10:51:02 -0300

    create folder routes and add new route to send a auth link

--- src/env.ts
+++ src/env.ts
@@ -1,6 +1,8 @@
 import { z } from 'zod'
 
 const envSchema = z.object({
+  AUTH_REDIRECT_URL: z.string().url().min(1),
+  API_BASE_URL: z.string().url().min(1),
   DATABASE_URL: z.string().url().min(1),
 })
 
--- src/http/server.ts
+++ src/http/server.ts
@@ -1,40 +1,9 @@
-import { Elysia, t } from 'elysia'
-import { db } from '../db/connection'
-import { restaurants, users } from '../db/schema'
+import { Elysia } from 'elysia'
 
-const app = new Elysia().post(
-  '/restaurants',
-  async ({ body, set }) => {
-    const { restaurantName, managerName, email, phone } = body
+import { registerRestaurant } from './routes/register-restaurant'
+import { sendAuthLink } from './routes/send-auth-link'
 
-    const [manager] = await db
-      .insert(users)
-      .values({
-        name: managerName,
-        email,
-        phone,
-        role: 'manager',
-      })
-      .returning({
-        id: users.id,
-      })
-
-    await db.insert(restaurants).values({
-      name: restaurantName,
-      managerId: manager.id,
-    })
-
-    set.status = 204
-  },
-  {
-    body: t.Object({
-      restaurantName: t.String(),
-      managerName: t.String(),
-      email: t.String({ format: 'email' }),
-      phone: t.String(),
-    }),
-  },
-)
+const app = new Elysia().use(registerRestaurant).use(sendAuthLink)
 
 app.listen(3333, () => {
   console.log('HTTP server running')
