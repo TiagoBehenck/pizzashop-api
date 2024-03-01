daebfac05e800014f670343d27ee50ab760da100
Tiago Behenck dos Santos <tiago.behenck.santos@gmail.com>
2024-03-01 16:57:20 -0300

    setup JWT

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,9 +1,25 @@
-import { Elysia } from 'elysia'
+import { Elysia, t } from 'elysia'
+import jwt from '@elysiajs/jwt'
+import cookie from '@elysiajs/cookie'
 
 import { registerRestaurant } from './routes/register-restaurant'
 import { sendAuthLink } from './routes/send-auth-link'
 
-const app = new Elysia().use(registerRestaurant).use(sendAuthLink)
+import { env } from '../env'
+
+const app = new Elysia()
+  .use(
+    jwt({
+      secret: env.JWT_SECRET_KEY,
+      schema: t.Object({
+        sub: t.String(),
+        restaurantId: t.Optional(t.String()),
+      }),
+    }),
+  )
+  .use(cookie())
+  .use(registerRestaurant)
+  .use(sendAuthLink)
 
 app.listen(3333, () => {
   console.log('HTTP server running')
