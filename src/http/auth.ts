d75e31b52ef25e932b9d2bfb83ba59cb0658ac8b
Tiago Behenck dos Santos <tiago.behenck.santos@gmail.com>
2024-03-04 14:46:50 -0300

    create sign out route and move the function (sign in/out) to a derive function on auth.ts

--- src/http/auth.ts
+++ src/http/auth.ts
@@ -1,17 +1,34 @@
-import { Elysia, t } from 'elysia'
+import { Elysia, t, type Static } from 'elysia'
 import cookie from '@elysiajs/cookie'
 import jwt from '@elysiajs/jwt'
 
 import { env } from '../env'
 
+const jwtPayload = t.Object({
+  sub: t.String(),
+  restaurantId: t.Optional(t.String()),
+})
 export const auth = new Elysia()
   .use(
     jwt({
       secret: env.JWT_SECRET_KEY,
-      schema: t.Object({
-        sub: t.String(),
-        restaurantId: t.Optional(t.String()),
-      }),
+      schema: jwtPayload,
     }),
   )
   .use(cookie())
+  .derive(({ jwt, setCookie, removeCookie }) => {
+    return {
+      signUser: async (payload: Static<typeof jwtPayload>) => {
+        const token = await jwt.sign(payload)
+
+        setCookie('auth', token, {
+          httpOnly: true,
+          maxAge: 60 * 60 * 24 * 7, // 7 days
+          path: '/',
+        })
+      },
+      signOut: () => {
+        removeCookie('auth')
+      },
+    }
+  })
--- src/http/routes/authenticate-from-link.ts
+++ src/http/routes/authenticate-from-link.ts
@@ -10,7 +10,7 @@ import { auth } from '../auth'
 const ONE_WEEK_IN_DAYS = 7
 export const authenticateFromLink = new Elysia().use(auth).get(
   '/auth-links/authenticate',
-  async ({ query, jwt, setCookie, set }) => {
+  async ({ query, signUser, set }) => {
     const { code, redirect } = query
 
     const authLinkFromCode = await db.query.authLinks.findFirst({
@@ -38,15 +38,9 @@ export const authenticateFromLink = new Elysia().use(auth).get(
       },
     })
 
-    const token = await jwt.sign({
-      sub: authLinkFromCode.userId,
+    await signUser({
+      sub: authLinkFromCode.id,
       restaurantId: managedRestaurant?.id,
-    })
-
-    setCookie('auth', token, {
-      httpOnly: true,
-      maxAge: 60 * 60 * 24 * 7, // 7 days
-      path: '/',
     })
 
     await db.delete(authLinks).where(eq(authLinks.code, code))
