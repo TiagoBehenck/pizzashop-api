ef532cf93db68f9b362ca1a861acc07c3f8911a3
Tiago Behenck dos Santos <tiago.behenck.santos@gmail.com>
2024-04-01 15:09:22 -0300

    create route to get order

---
+++ src/http/routes/get-orders.ts
@@ -0,0 +1,63 @@
+import { Elysia, t } from 'elysia'
+
+import { auth } from '../auth'
+import { db } from '../../db/connection'
+import { UnauthorizedError } from '../errors/unauthorized-error'
+import { createSelectSchema } from 'drizzle-typebox'
+import { and, count, eq, getTableColumns, ilike } from 'drizzle-orm'
+import { orders, users } from '../../db/schema'
+
+export const getOrders = new Elysia().use(auth).get(
+  '/orders',
+  async ({ getCurrentUser, query }) => {
+    const { restaurantId } = await getCurrentUser()
+    const { customerName, orderId, status, pageIndex } = query
+
+    if (!restaurantId) {
+      throw new UnauthorizedError()
+    }
+
+    const orderTableColumns = getTableColumns(orders)
+
+    const baseQuery = db
+      .select(orderTableColumns)
+      .from(orders)
+      .innerJoin(users, eq(users.id, orders.customerId))
+      .where(
+        and(
+          eq(orders.restaurantId, restaurantId),
+          customerName ? ilike(users.name, `%${customerName}%`) : undefined,
+          orderId ? ilike(orders.id, `%${orderId}%`) : undefined,
+          status ? ilike(orders.status, status) : undefined,
+        ),
+      )
+
+    const [amountOfOrdersQuery, allOrders] = await Promise.all([
+      db.select({ count: count() }).from(baseQuery.as('baseQuery')),
+      db
+        .select()
+        .from(baseQuery.as('baseQuery'))
+        .offset(pageIndex * 10)
+        .limit(10),
+    ])
+
+    const amountOfOrders = amountOfOrdersQuery[0].count
+
+    return {
+      orders: allOrders,
+      meta: {
+        pageIndex,
+        perPage: 10,
+        totalCount: amountOfOrders,
+      },
+    }
+  },
+  {
+    query: t.Object({
+      customerName: t.Optional(t.String()),
+      orderId: t.Optional(t.String()),
+      status: t.Optional(createSelectSchema(orders).properties.status),
+      pageIndex: t.Numeric({ minimum: 0 }),
+    }),
+  },
+)
